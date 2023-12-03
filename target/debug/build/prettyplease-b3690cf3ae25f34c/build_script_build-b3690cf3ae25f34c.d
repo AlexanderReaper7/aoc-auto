03705740b29eca6318e2c85fb44a089a5bab9c23
Alexander Öberg <stativetroller7@gmail.com>
2023-12-04 00:01:06 +0100

    first commit

---
+++ target/debug/build/prettyplease-b3690cf3ae25f34c/build_script_build-b3690cf3ae25f34c.d
@@ -0,0 +1,7 @@
+c:\Projects\aoc-auto\target\debug\build\prettyplease-b3690cf3ae25f34c\build_script_build-b3690cf3ae25f34c.exe: C:\Users\Alexander\.cargo\registry\src\index.crates.io-6f17d22bba15001f\prettyplease-0.2.15\build.rs
+
+c:\Projects\aoc-auto\target\debug\build\prettyplease-b3690cf3ae25f34c\build_script_build-b3690cf3ae25f34c.d: C:\Users\Alexander\.cargo\registry\src\index.crates.io-6f17d22bba15001f\prettyplease-0.2.15\build.rs
+
+C:\Users\Alexander\.cargo\registry\src\index.crates.io-6f17d22bba15001f\prettyplease-0.2.15\build.rs:
+
+# env-dep:CARGO_PKG_VERSION=0.2.15
---
+++ target/debug/deps/aoc_auto-4010834a6585fde3.d
@@ -0,0 +1,5 @@
+c:\Projects\aoc-auto\target\debug\deps\aoc_auto-4010834a6585fde3.exe: src\lib.rs
+
+c:\Projects\aoc-auto\target\debug\deps\aoc_auto-4010834a6585fde3.d: src\lib.rs
+
+src\lib.rs:
